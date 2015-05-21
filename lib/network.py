43047399afb517781aee3911cc703e6329532beb
underc0de <iegormundo@hotmail.com>
2015-05-22 01:20:11 +0200

    Organize the network component

--- lib/network.py
+++ lib/network.py
@@ -2,9 +2,36 @@ import random
 import math
 from copy import copy
 
-class Network:
-	def __init__(self):
-		print u'Initializing neural network...'
+class Network(object):
+    def __init__(self):
+        print u'Initializing network...'
+
+        self._events = {}
+        self.on = self.register
+        self.off = self.unregister
+
+    def _get_listeners(self, event):
+        if not self._events.get(event, False):
+            self._events[event] = []
+        return self._events[event]
+
+    def register(self, event, fn=None):
+        if not fn in self._get_listeners(event) and fn:
+            self._events[event].append(fn)
+        if not fn:
+            def add(fn):
+                self._events[event].append(fn)
+            return add
+
+    def unregister(self, event, fn):
+        i = fn in self._get_listeners(event)
+        print(i)
+
+    def emit(self, event, *args):
+        for listener in self._get_listeners(event):
+            if listener(*args) is True:
+                self.unregister(event, listener)
+        return self
 
 
 class NeuralNetwork(object):
@@ -14,6 +41,8 @@ class NeuralNetwork(object):
     t = 1
     
     def __init__(self, neurals_counts, learning_rate=0.2, momentum=0, verbosity=True, weights=None):
+        print u'Initializing neural network...'
+
         self.weights = []
         self.learning_rate = learning_rate
         self.neurals_counts = neurals_counts
